55560a8f4dc89fb43aa89bc5ca7ae500ffc69783
Muthu V <132546801+Muthu4558@users.noreply.github.com>
2024-12-31 17:31:16 +0530

    Add files via upload

---
+++ backend/backend.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+//mongodb connection
+const mongoUrl =
+  "mongodb+srv://muthukdm45:12345@muthu.wpwai.mongodb.net/";
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((e) => console.log(e));
+
+//importing schema
+require("./imageDetails");
+const Images = mongoose.model("ImageDetails");
+
+app.get("/", async (req, res) => {
+  res.send("Success!!!!!!");
+});
+
+app.listen(5000, () => {
+  console.log("Server Started");
+});
+
+//////////////////////////////////////////////////////////////
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../src/images/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload-image", upload.single("image"), async (req, res) => {
+  console.log(req.body);
+  const imageName = req.file.filename;
+
+  try {
+    await Images.create({ image: imageName });
+    res.json({ status: "ok" });
+  } catch (error) {
+    res.json({ status: error });
+  }
+});
+
+app.get("/get-image", async (req, res) => {
+  try {
+    Images.find({}).then((data) => {
+      res.send({ status: "ok", data: data });
+    });
+  } catch (error) {
+    res.json({ status: error });
+  }
+});
